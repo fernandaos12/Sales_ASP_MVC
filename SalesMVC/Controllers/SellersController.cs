118e70d6a125dbf01792cc2de8fce6e8a608d9ab
fernandaos12 <fernandaos12@gmail.com>
2021-05-19 16:59:07 -0300

    SellerService and basic FindAll

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesMVC.Services;
 
 namespace SalesMVC.Controllers
 {
     public class SellersController : Controller
     {
+        //criando dependencia do seller service
+        private readonly SellerService _sellerservice;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerservice = sellerService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerservice.FindAll();
+
+            return View(list);
         }
     }
 }
