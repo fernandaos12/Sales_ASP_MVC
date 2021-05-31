1f7be6bd1708ce07f7d96da18363edf07b997a3b
fernandaos12 <fernandaos12@gmail.com>
2021-05-31 14:23:01 -0300

    Annotations

--- SalesMVC/Models/Seller.cs
+++ SalesMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesMVC.Models
@@ -8,8 +9,17 @@ namespace SalesMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+       
+        [Display(Name = "Birth Date")] //customizando a label da table
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DateTime { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }  //foreignkey para nao ficar nulo
